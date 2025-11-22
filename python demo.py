8e8a3908a5496af79e1a6e1febabfd3d543145d5
Vedant-2407 <davevedant10@gmail.com>
2025-11-23 09:00:59 +1300

    Add files via upload

---
+++ python demo.py
@@ -0,0 +1,18 @@
+#storing a string
+Name = "John Doe"
+print(Name)
+#storing an int
+Age = 25
+print(Age)
+#storing List
+Skills = ["python","sql","power"]
+print(Skills)
+#storing a tuple can use multiple data types
+Education = ("BSc Computer",2025)
+print(Education)
+#storing a dict
+Contact_Details = {"email": "davevedant10@gmail.com","phone": "7202989370"}
+print(Contact_Details)
+#storing a set you can't use the same value in a set even if you do it wont show error but it wont be printed
+certifications = {"Azure", "AWS", "Azure"}
+print(certifications)
